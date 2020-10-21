0eec3fd1172c1f37383de2f32bafed3926663091
Max Wu <littlemengying@gmail.com>
2020-10-20 17:55:59 -0700

    add count

--- guess.py
+++ guess.py
@@ -8,13 +8,17 @@ Created on Tue Oct 20 14:48:56 2020
 
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+    count = count + 1 # can also be written as count += 1 
     num = int(input("Please guess a number between 1 and 100: "))
     if r == num:
         print ("Congratulations. You guessed it right!")
+        print ("This is the", count, "time(s) you tried")
         break
     elif r > num:
-        print("Too bad, it's smaller than the answer.")
+        print("It's smaller than the answer.")
     elif r < num:
-        print("Too bad, it's bigger than the answer.")
-    
+        print("It's bigger than the answer.")
+    print ("This is the", count, "time(s) you tried")
+ 
